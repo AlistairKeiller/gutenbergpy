40e22c3f0cdcd2822c752c9019efa9094c730d7f
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-05-04 19:05:03 -0700

    Update rdfparser.py

--- gutenbergpy/parse/rdfparser.py
+++ gutenbergpy/parse/rdfparser.py
@@ -38,6 +38,8 @@ class RdfParser:
         total = len(dirs)
 
         for idx, dir in enumerate(dirs):
+            if idx == 14419:
+                continue
             processing_str = "Processing progress: %d / %d" % (idx,total)
             Utils.update_progress_bar(processing_str,idx,total)
             file_path = path.join(GutenbergCacheSettings.CACHE_RDF_UNPACK_DIRECTORY,dir,'pg%s.rdf'%(dir))
